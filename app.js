9d5eaeadead9f0dd53b1e9ed8265325a81c7497e
ncdjknkedah <ncdjkn.kdh@gmail.com>
2026-05-14 11:27:04 +0800

    Update app.js

--- app.js
+++ app.js
@@ -264,6 +264,7 @@ function renderDemographicsChart(responses) {
 }
 
 function renderEvaluationChart(responses) {
+    // 1. Ratings Bar Chart
     const ctx = document.getElementById('evaluationChart').getContext('2d');
     if (charts.eval) charts.eval.destroy();
 
@@ -303,6 +304,49 @@ function renderEvaluationChart(responses) {
                     color: '#34495e',
                     font: { weight: 'bold' },
                     formatter: (value) => value.toFixed(2)
+                }
+            }
+        }
+    });
+
+    // 2. Binary Questions (Pie/Donut)
+    renderBinaryChart('amalanChart', 'amalan_kendiri', responses);
+    renderBinaryChart('logoChart', 'lihat_logo', responses);
+}
+
+function renderBinaryChart(id, key, responses) {
+    const ctx = document.getElementById(id).getContext('2d');
+    if (charts[id]) charts[id].destroy();
+
+    const counts = responses.reduce((acc, curr) => {
+        const val = curr[key] || 'Tiada Data';
+        acc[val] = (acc[val] || 0) + 1;
+        return acc;
+    }, {});
+
+    charts[id] = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: Object.keys(counts),
+            datasets: [{
+                data: Object.values(counts),
+                backgroundColor: ['#2ecc71', '#e74c3c', '#95a5a6']
+            }]
+        },
+        options: {
+            maintainAspectRatio: false,
+            plugins: {
+                legend: { position: 'bottom', labels: { boxWidth: 10, font: { size: 10 } } },
+                datalabels: {
+                    color: '#fff',
+                    font: { weight: 'bold', size: 10 },
+                    formatter: (value, ctx) => {
+                        let sum = 0;
+                        let dataArr = ctx.chart.data.datasets[0].data;
+                        dataArr.map(data => { sum += data; });
+                        let percentage = (value * 100 / sum).toFixed(1) + "%";
+                        return value > 0 ? `${value}\n(${percentage})` : '';
+                    }
                 }
             }
         }
